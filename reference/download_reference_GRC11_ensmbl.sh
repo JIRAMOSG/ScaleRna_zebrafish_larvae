87c2f41492571e410117e7605bfc1ef438405fff
Johana Itzel <92237528+JIRAMOSG@users.noreply.github.com>
2026-04-10 18:45:09 +0100

    Add script to download GRCz11 reference files

---
+++ reference/download_reference_GRC11_ensmbl.sh
@@ -0,0 +1,2 @@
+wget https://ftp.ensembl.org/pub/release-115/fasta/danio_rerio/dna/Danio_rerio.GRCz11.dna.primary_assembly.fa.gz
+wget https://ftp.ensembl.org/pub/release-115/gtf/danio_rerio/Danio_rerio.GRCz11.115.gtf.gz
