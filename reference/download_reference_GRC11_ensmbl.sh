73844d4b093ddb9a2e6480c74221aaecc702a857
Johana Itzel <92237528+JIRAMOSG@users.noreply.github.com>
2026-04-10 18:46:30 +0100

    Download and unzip GRCz11 reference files

--- reference/download_reference_GRC11_ensmbl.sh
+++ reference/download_reference_GRC11_ensmbl.sh
@@ -1,2 +1,5 @@
+cd /well/ludwig/users/lay823/larvae_nsc/reference
 wget https://ftp.ensembl.org/pub/release-115/fasta/danio_rerio/dna/Danio_rerio.GRCz11.dna.primary_assembly.fa.gz
 wget https://ftp.ensembl.org/pub/release-115/gtf/danio_rerio/Danio_rerio.GRCz11.115.gtf.gz
+gunzip Danio_rerio.GRCz11.dna.primary_assembly.fa.gz
+gunzip Danio_rerio.GRCz11.115.gtf.gz
