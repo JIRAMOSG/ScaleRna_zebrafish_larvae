ca8b42e01a7719bc1b1bd7caa7565a92b5c57ef3
Johana Itzel <92237528+JIRAMOSG@users.noreply.github.com>
2026-04-13 17:39:40 +0100

    Modify srun commands and test run configurations
    Updated resource allocation for srun commands and adjusted test run parameters.

--- Installing&running/dependencies_pipeline.sh
+++ Installing&running/dependencies_pipeline.sh
@@ -9,12 +9,9 @@ conda deactivate
 # squeue -u lay823
 # srun -p short --nodes=1 --ntasks-per-node=1 --cpus-per-task=12  --mem=100G --time=24:00:00 --pty /bin/bash -i 
 
-srun -p short --nodes=1 --ntasks-per-node=1 --cpus-per-task=6  --mem=60G --time=24:00:00 --pty /bin/bash -i 
 
 # Clone github
 git clone https://github.com/ScaleBio/ScaleRna.git
-
-
 # Downloading test run data
 conda create -n s3env python=3.10
 conda activate s3env
@@ -24,7 +21,7 @@ aws s3 sync s3://scale.pub/testData/rna/202506_quantumV1/fastq fastq --no-sign-r
 conda deactivate
 
 #TEST
-srun -p short --nodes=2 --ntasks-per-node=1 --cpus-per-task=8  --mem=150G --time=24:00:00 --pty /bin/bash -i
+srun -p short --nodes=1 --ntasks-per-node=1 --cpus-per-task=16  --mem=150G --time=24:00:00 --pty /bin/bash -i
 module load Nextflow/24.04.2
 module load Java/11.0.20
 
@@ -34,8 +31,6 @@ nextflow run /well/ludwig/users/lay823/larvae_nsc/ScaleRna/ \
   --fastqDir /well/ludwig/users/lay823/larvae_nsc/test_inst/fastq \
   --outDir /well/ludwig/users/lay823/larvae_nsc/test_inst/output
 
-#Pulling Singularity image docker://public.ecr.aws/o5l3p3e4/scalerna:2025-04-16-160428 [cache /gpfs3/well/ludwig/users/lay823/larvae_nsc/ScaleRna/work/singularity/public.ecr.aws-o5l3p3e4-scalerna-2025-04-16-160428.img]
-#WARN: Singularity cache directory has not been defined -- Remote image will be stored in the path: /gpfs3/well/ludwig/users/lay823/larvae_nsc/ScaleRna/work/singularity -- Use the environment variable NXF_SINGULARITY_CACHEDIR to specify a different location
 
 
 
