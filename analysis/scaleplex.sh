803d507b5f149115c0762167bb71437b8928b998
Johana Itzel <92237528+JIRAMOSG@users.noreply.github.com>
2026-04-14 20:00:25 +0100

    Update scaleplex.sh

--- analysis/scaleplex.sh
+++ analysis/scaleplex.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=scaleplex
 #SBATCH --output=job.%j.out
+#SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=18
-#SBATCH --mem=200G
-#SBATCH --time=40:00:00
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=250G
+#SBATCH --time=50:00:00
 cd /well/ludwig/users/lay823/larvae_nsc
 module load Nextflow/24.04.2
 module load Java/11.0.20
 
-nextflow run ScaleBio/ScaleRna \
+nextflow run /well/ludwig/users/lay823/larvae_nsc/ScaleRna \
     --runFolder /well/ludwig/users/lay823/larvae_nsc/data/bcl/260327_LH00452_0387_B23JH7HLT3 \
     --profile singularity \
     --samples /well/ludwig/users/lay823/larvae_nsc/data/samples.csv \
@@ -18,3 +19,4 @@ nextflow run ScaleBio/ScaleRna \
     --outDir results
 
 
+# sbatch scaleplex.sh
