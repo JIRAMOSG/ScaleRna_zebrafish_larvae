c7d796f8949c5c227a8e1e915010ff01125db090
Johana Itzel <92237528+JIRAMOSG@users.noreply.github.com>
2026-04-15 17:57:20 +0100

    Remove SLURM directives from scaleplex_masterjob.sh
    Removed SLURM job configuration from the script.

--- analysis/scaleplex_masterjob.sh
+++ analysis/scaleplex_masterjob.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-#SBATCH -A ludwig.prj 
-#SBATCH --job-name=scaleplex
-#SBATCH -o job.%j.out 
-#SBATCH -e job.%j.err 
-#SBATCH -p long
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
-#SBATCH --mem=150G
-
 cd /well/ludwig/users/lay823/larvae_nsc
 module load Nextflow/24.04.2
 module load Java/11.0.20
